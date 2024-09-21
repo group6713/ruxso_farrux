2abb074baabebe31032b7ff42dd4c6f410d1eeb0
Ruxsora <Ruxsora@gmail.com>
2024-09-21 20:17:24 +0500

    Ruhsora va Farruh

--- src/Components/Ruxsora/ruxsoo.jsx
+++ src/Components/Ruxsora/ruxsoo.jsx
@@ -4,8 +4,81 @@ import "./ruxsoo.css"
 const Ruxsora = ()=>{
     return(
      <div>
-        <div className="container">
+
+<div className="container900">
+<img  style={{width:'1600px', height:'900px'}} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvsNNgfsLj5aZgFPzSl6PQ2cjOSLdOFHhBRw&s" alt="" />
+</div>
+
+        <div style={{display:'flex',justifyContent:'center'}} className="container">
           <h1>A comprehesive <br /> set of service</h1>
+          <div style={{marginTop:'30px', marginLeft:'100px'}} className="box">
+          <button style={{width:'100px',height:'50px',background:'orange'}}>Get a quote</button>
+          <button style={{width:'100px',height:'50px', marginLeft:'40px',}}>Browse </button>
+          </div>
+        </div>
+
+
+        <div className="container2" style={{display:'flex',justifyContent:'space-around'}}>
+<div className="box">
+  <h1>Project Planning</h1>
+  <p>Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Provident repudiandae <br /> enim deleniti, ex nemo ratione  laborum <br />  </p>
+</div>
+
+<div className="box">
+  <h1>Project Planning</h1>
+  <p>Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Provident repudiandae <br /> enim deleniti, ex nemo ratione  laborum <br />  </p>
+</div>
+
+<div className="box">
+  <h1>Project Planning</h1>
+  <p>Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Provident repudiandae <br /> enim deleniti, ex nemo ratione  laborum <br />  </p>
+</div>
+
+
+
+
+        </div>
+
+
+
+        <div className="container2" style={{display:'flex',justifyContent:'space-around'}}>
+<div className="box">
+  <h1>Project Planning</h1>
+  <p>Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Provident repudiandae <br /> enim deleniti, ex nemo ratione  laborum <br />  </p>
+</div>
+
+<div className="box">
+  <h1>Project Planning</h1>
+  <p>Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Provident repudiandae <br /> enim deleniti, ex nemo ratione  laborum <br />  </p>
+</div>
+
+<div className="box">
+  <h1>Project Planning</h1>
+  <p>Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Provident repudiandae <br /> enim deleniti, ex nemo ratione  laborum <br />  </p>
+</div>
+
+
+
+
+        </div>
+
+
+        <div className="container3">
+          <div className="box2">
+             <img style={{width:'1500px', height:'600px', marginTop:'50px'}} src="https://st2.depositphotos.com/1001335/9340/i/450/depositphotos_93401314-stock-photo-concept-of-construction-and-design.jpg" alt="" />
+          </div>
+        </div>
+
+
+        <div  className="container4">
+          <div tyle={{display:'flex'}}  className="box3">
+            <img src="https://img.freepik.com/free-vector/organic-flat-engineers-working-on-construction-site-illustration_23-2148909837.jpg" alt="" />
+            <h1>Lorem ipsum <br /> dolor sit, amet <br /> consectetur <br />
+             </h1>
+             <div className="box6">
+             <p>Lorem ipsum dolor sit amet consectetur  <br />adipisicing elit. In harum, a pariatur sequi labore modi alias <br /> quos ab illo error totam iste doloremque, aut officiis! Hic <br /> corporis dolores atque quaerat?<br /></p>
+             </div>
+          </div>
         </div>
       </div>
     )
