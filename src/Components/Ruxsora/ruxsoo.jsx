f9171964eef6e90002f77ec571ee0a39664f4cb2
Ruxsora <Ruxsora@gmail.com>
2024-09-21 19:38:49 +0500

    farrux boshlla

---
+++ src/Components/Ruxsora/ruxsoo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./ruxsoo.css"
+
+const Ruxsora = ()=>{
+    return(
+     <div>
+        <div className="container">
+          <h1>A comprehesive <br /> set of service</h1>
+        </div>
+      </div>
+    )
+    }
+    export default Ruxsora;
