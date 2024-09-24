62a1b8d6b7c3ef803a9d66daacf62ebc61c464a5
Farruh Sadullayev <sadullayevf923@gmail.com>
2024-09-24 19:56:20 +0500

    man ishimni tugatdim

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react"
 import Ruxsora from "./Components/Ruxsora/ruxsoo"
+import Farruh from "./Components/Farrux/farrux"
+import navbar from "./Components/Navbar/navbar"
 function App() {
 
   return (
     <>
+      <Navbar/>
      <Ruxsora/>
     <Farrux/>
     </>
   )
 }
-
 export default App
