04131fad8e448b565b4360ee83501ee04bec983b
Ruxsora <Ruxsora@gmail.com>
2024-09-24 19:31:15 +0500

    ishimmi tugattim

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react"
 import Ruxsora from "./Components/Ruxsora/ruxsoo"
+import Farrux from "./Components/Farrux/farrux"
+
 function App() {
 
   return (
     <>
+    
      <Ruxsora/>
+    <Farrux/>
     </>
   )
 }
